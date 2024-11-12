efb7fa8570586f3637e558b20340ba3e523e5bef
Aditya <aditya@fisip.net>
2024-11-12 11:36:51 +0700

    update highligh visual mode

--- lua/aditya/plugins/colorscheme.lua
+++ lua/aditya/plugins/colorscheme.lua
@@ -1,28 +1,31 @@
-
 return {
-    {
-        "briones-gabriel/darcula-solid.nvim",
-        priority = 1000,
-        dependencies = { "rktjmp/lush.nvim" },
-        config = function()
-            -- Apply Darcula Solid colorscheme
-            vim.cmd("colorscheme darcula-solid")
+	{
+		"briones-gabriel/darcula-solid.nvim",
+		priority = 1000,
+		dependencies = { "rktjmp/lush.nvim" },
+		config = function()
+			-- Apply Darcula Solid colorscheme
+			vim.cmd("colorscheme darcula-solid")
 
-            -- Set NvimTree folder text colors to white
-            vim.cmd([[
+			-- Set NvimTree folder text colors to white
+			vim.cmd([[
                 highlight NvimTreeFolderName guifg=#c4c2c2
                 highlight NvimTreeOpenedFolderName guifg=#c4c2c2
                 highlight NvimTreeEmptyFolderName guifg=#c4c2c2
             ]])
-        end,
-    },
-    {
-        "nvim-tree/nvim-tree.lua",
-        config = function()
-            require("nvim-tree").setup({
-                -- NvimTree configuration goes here
-            })
-        end,
-    },
-}
 
+			-- Set Visual mode selection background to blue
+			vim.cmd([[
+                highlight Visual guibg=#004187 
+            ]])
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup({
+				-- NvimTree configuration goes here
+			})
+		end,
+	},
+}
